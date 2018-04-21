061d4d0ac8cdd1ad3d4d3073898b5468b9ee7f01
DE165874 <Caner.Koc-CIC.Germany@ibm.com>
2018-04-21 23:14:25 +0200

    test6 int nummer3

--- test-koma-con/src/com/kocak/dom/test6.java
+++ test-koma-con/src/com/kocak/dom/test6.java
@@ -3,6 +3,7 @@ package com.kocak.dom;
 public class test6 {
 	private int nummer;
 	private int nummer2;
+	private int nummer3;
 	
 	public static void main (String [] args) {
 		System.out.println("hello world");
